a17d98cef0c1e4fb10c65f3409fbcbf6f52b1a15
folows1 <michelribeirocorrea@gmail.com>
2023-03-12 23:46:10 -0300

    footer api credits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,36 @@ import LogoTwitter from "../assets/icons8-twitter.svg";
 
 function Footer() {
   return (
-    <footer className="text-center bg-yellow-900 w-full">
-      <div className="flex flex-col items-center justify-center p-3 md:flex-row md:justify-evenly">
+    <footer className="text-center bg-yellow-900 w-full p-3">
+      <p className="font-bold text-xs mb-3">
+        Esse jogo utiliza a API pública do{" "}
         <a
-          href="https://github.com/folows1"
+          href="https://dog.ceo/dog-api/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white underline"
+        >
+          Dog API
+        </a>
+        .
+      </p>
+      <div className="flex flex-col items-center justify-center md:flex-row md:justify-evenly">
+        <a
+          href="https://twitter.com/folows1"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white"
+        >
+          <img
+            src={LogoTwitter}
+            alt="Linkedin"
+            className="w-[40px] h-[40px] inline-block"
+          />
+          <p className="text-white text-sm ml-2 inline-block">/folows1</p>
+        </a>
+
+        <a
+          href="https://github.com/folows1/dog-quiz"
           target="_blank"
           rel="noreferrer"
           className="text-white"
@@ -32,20 +58,6 @@ function Footer() {
             className="w-[40px] h-[40px] inline-block"
           />
           <p className="text-white text-sm ml-2 inline-block">Michel Ribeiro</p>
-        </a>
-
-        <a
-          href="https://twitter.com/folows1"
-          target="_blank"
-          rel="noreferrer"
-          className="text-white"
-        >
-          <img
-            src={LogoTwitter}
-            alt="Linkedin"
-            className="w-[40px] h-[40px] inline-block"
-          />
-          <p className="text-white text-sm ml-2 inline-block">/folows1</p>
         </a>
       </div>
     </footer>
